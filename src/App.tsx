305d6b9acec1662ee760377b082f9925f1c53c0c
jacob-oreilly <jakeor45@gmail.com>
2022-10-29 16:54:31 -0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import * as Tone from 'tone';
 function App() {
+
+  
+   //Initializing Audio
+   const initTone = async () => {
+    await Tone.start()
+    console.log('audio is ready')
+  } 
+
+  //Audio source
+
+  //Do thing
+  
+
+  //turn on sound (in this case we are generating but should have a function
+  // to listen to incoming audio src eventually.)
+  const playSound = () => {
+    const fmSynth = new Tone.FMSynth().toDestination();
+    fmSynth.triggerAttackRelease("C5", "4n");
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        distortion pedal
       </header>
+      <div className='container'>
+        <button onClick={playSound}>
+          Click Me!
+        </button>
+      </div>
     </div>
   );
 }
